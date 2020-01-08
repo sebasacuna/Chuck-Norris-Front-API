9d3acf326e965eefc7392b600cbabb37765eec39
jarenasm@sodimac.cl <sebastianacuna.it@gmail.com>
2020-01-08 18:58:14 -0300

    delete about ref

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-
+console.log(__dirname);
 app.use(express.static(__dirname + '/dist/chuckfrontapi'));
 
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/chuckfrontapi/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
