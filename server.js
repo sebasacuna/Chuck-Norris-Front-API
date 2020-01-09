73fc4c49bcd14482f5b59de97528b9d437f20efc
jarenasm@sodimac.cl <sebastianacuna.it@gmail.com>
2020-01-09 15:19:18 -0300

    add log

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var count = 0;
+
 console.log(__dirname);
 app.use(express.static(__dirname + '/dist/chuckfrontapi'));
 
 app.get('/*', function (req, res) {
+  count++;
+  console.log('somebody get a joke :) - number of jokes get: '+count);
   res.sendFile(path.join(__dirname + '/dist/chuckfrontapi/index.html'));
 });
 
